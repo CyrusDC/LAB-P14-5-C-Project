f62043f9ac399a4589898e88130977db8c218bfb
CyrusDC <cyrusdelcarmen@gmail.com>
2025-11-09 22:27:43 +0800

    Test Solution commit.

---
+++ CMS/Main.c
@@ -0,0 +1,12 @@
+
+// This File will handle the user input and deal with any weird user inputs
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(void) 
+{
+	printf("Welcome to the Class Management System (CMS)");
+
+}
